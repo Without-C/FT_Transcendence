3baa49f2d067b7cf086561b7868e73cb5df53620
Youngho Cho <dominoyh5@gmail.com>
2025-02-10 19:09:02 +0900

    feat: add Player class

--- backend-ping-pong/src/routes/ping-pong/duel/index.ts
+++ backend-ping-pong/src/routes/ping-pong/duel/index.ts
@@ -2,13 +2,30 @@ import { FastifyPluginAsync } from "fastify"
 import websocket from '@fastify/websocket'
 import { v4 as uuidv4 } from 'uuid'
 
+class Player {
+    public id: string;
+    public ws: any;
+
+    constructor(ws: any) {
+        this.id = 'player-' + uuidv4();
+        this.ws = ws;
+    }
+
+    public send(message: any): void {
+        this.ws.send(JSON.stringify(message))
+    }
+}
+
 const example: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
     await fastify.register(websocket)
 
     fastify.get('/ws', { websocket: true }, async (ws) => {
+        const player = new Player(ws);
+
         ws.on('message', async (message) => {
-            const msg = message.toString()
-            await ws.send(msg)
+            const data = JSON.parse(message.toString())
+            player.send(data)
+            player.send(player.id)
         })
 
         ws.on('close', async () => {
