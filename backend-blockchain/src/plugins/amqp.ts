edfa963ebb4b0f787f280dc6308945f92f0d2069
Youngho Cho <dominoyh5@gmail.com>
2025-02-14 18:14:10 +0900

    docs: add amqp msg example

--- backend-blockchain/src/plugins/amqp.ts
+++ backend-blockchain/src/plugins/amqp.ts
@@ -8,10 +8,38 @@ declare module 'fastify' {
     }
 }
 
+// const example_msg1 = {
+//     "game_end_reason": "normal",    // 게임이 정상적으로 종료되었음
+//     "player1": {
+//         "id": "player-577a2d57-005f-42d7-a855-5d352d193cff",
+//         "round_score": 2,
+//         "result": "winner",
+//     }, "player2": {
+//         "id": "player-a81108f7-8ff7-4654-bc52-dbcd4dfff245",
+//         "round_score": 1,
+//         "result": "loser",
+//     },
+// };
+
+// const example_msg2 = {
+//     "game_end_reason": "player_disconnected",   // 상대방이 중간에 나가 게임이 종료되었음
+//     "player1": {
+//         "id": "player-95883643-eaa0-4a0d-a330-ad88343b8d39",
+//         "round_score": 0,
+//         "result": "loser",
+//     },
+//     "player2": {
+//         "id": "player-fccde446-6fb7-4575-924e-9772ddd900f2",
+//         "round_score": 1,
+//         "result": "winner",
+//     },
+// };
+
 // 메시지(게임 결과)를 받을 때마다 호출되는 함수
 function handleMessage(fastify: FastifyInstance, channel: amqp.Channel, msg: amqp.ConsumeMessage) {
     // 메시지 처리
     fastify.log.info("%s", msg.content.toString());
+    // fastify.log.info("%s", msg.content.toJSON());
 
     // 게임 결과를 블록체인에 무사히 올렸으면 메시지 큐에게 ack 전송
     channel.ack(msg);
