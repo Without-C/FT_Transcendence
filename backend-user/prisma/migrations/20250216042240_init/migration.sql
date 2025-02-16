1d61dc300d2c884cd266d16bee2795078dd9fc31
Youngho Cho <dominoyh5@gmail.com>
2025-02-16 13:25:08 +0900

    feat: add prisma scheme

---
+++ backend-user/prisma/migrations/20250216042240_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "username" TEXT,
+    "avatar_url" TEXT,
+    "oauth_id_42" TEXT,
+    "oauth_id_github" TEXT,
+    "oauth_id_google" TEXT
+);
