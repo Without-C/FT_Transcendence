68e88f473dc9102832f07f1c1c93bc5900b780fc
Youngho Cho <dominoyh5@gmail.com>
2025-02-23 11:52:05 +0900

    fix: replace elastic password to ENV

--- elk/kibana/docker-entrypoint.sh
+++ elk/kibana/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 # Change password for kibana_system
 curl -k -X POST "https://elasticsearch:9200/_security/user/kibana_system/_password" \
   -H "Content-Type: application/json" \
-  -u elastic:elastic \
+  -u elastic:$ELASTIC_PASSWORD \
   -d '{"password": "kibana"}'
 
 # Run Kibana
@@ -19,7 +19,7 @@ done
 # Import a dashboard
 curl -X POST "http://localhost:5601/api/saved_objects/_import?overwrite=true" \
   -H "kbn-xsrf: true" \
-  -u elastic:elastic \
+  -u elastic:$ELASTIC_PASSWORD \
   --form file=@/elk/kibana/dashboard-proxy.ndjson
 
 wait $KIBANA_PID
