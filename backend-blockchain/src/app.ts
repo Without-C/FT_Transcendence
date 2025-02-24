693386c1369093442bf8da1795d0b281ed45625b
Youngho Cho <dominoyh5@gmail.com>
2025-02-24 17:16:10 +0900

    feat: add log config for backend-blockchain

--- backend-blockchain/src/app.ts
+++ backend-blockchain/src/app.ts
@@ -1,23 +1,34 @@
 import * as path from 'node:path';
-import AutoLoad, {AutoloadPluginOptions} from '@fastify/autoload';
-import { FastifyPluginAsync } from 'fastify';
+import AutoLoad, { AutoloadPluginOptions } from '@fastify/autoload';
+import { FastifyPluginAsync, FastifyServerOptions } from 'fastify';
 import { fileURLToPath } from 'node:url'
+import { pino } from 'pino'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export type AppOptions = {
   // Place your custom options for app below here.
-} & Partial<AutoloadPluginOptions>;
+} & FastifyServerOptions & Partial<AutoloadPluginOptions>;
 
 
 // Pass --options via CLI arguments in command to enable these options.
 const options: AppOptions = {
+  logger: false,
+  loggerInstance: pino(pino.transport({
+    targets: [{
+      level: 'info',
+      target: 'pino-pretty'
+    }, {
+      level: 'trace',
+      target: 'pino/file',
+      options: { destination: '/var/log/backend-user/app.log' }
+    }]
+  })),
 }
-
 const app: FastifyPluginAsync<AppOptions> = async (
-    fastify,
-    opts
+  fastify,
+  opts
 ): Promise<void> => {
   // Place here your custom code!
 
