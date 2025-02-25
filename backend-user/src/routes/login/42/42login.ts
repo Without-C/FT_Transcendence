ef307fbfabd489b3eac38d9a493b3161f7648575
duckgii <shinyonggi@naver.com>
2025-02-25 14:58:38 +0900

    Style : delete comment

--- backend-user/src/routes/login/42/42login.ts
+++ backend-user/src/routes/login/42/42login.ts
@@ -1,15 +1,14 @@
 import { FastifyPluginAsync } from 'fastify'
-//import fp from 'fastify-plugin'
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get('/', async function (request, reply) {
+	fastify.get('/', async function (request, reply) {
 
-	const authUrl = `https://api.intra.42.fr/oauth/authorize?` +
-    `client_id=${process.env.OAUTH_UID_42 || ''}&` +
-    `redirect_uri=${encodeURIComponent(process.env.OAUTH_REDIRECT_42 || '')}&` +  // 기본값 추가
-    `response_type=code`
+		const authUrl = `https://api.intra.42.fr/oauth/authorize?` +
+			`client_id=${process.env.OAUTH_UID_42 || ''}&` +
+			`redirect_uri=${encodeURIComponent(process.env.OAUTH_REDIRECT_42 || '')}&` +
+			`response_type=code`
 
-  	reply.redirect(authUrl)
-  })
+		reply.redirect(authUrl)
+	})
 };
 export default root;
