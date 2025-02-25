628c26c726be93a0663683c9959c4f4315d680c5
Youngho Cho <dominoyh5@gmail.com>
2025-02-25 19:18:56 +0900

    feat: add retention, archive polity to elastic search

---
+++ elk/elasticsearch/docker-entrypoint.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+
+# Run Elasticsearch
+/usr/local/bin/docker-entrypoint.sh "$@" &
+ES_PID=$!
+
+# Wait Elasticsearch
+until curl -u "elastic:${ELASTIC_PASSWORD}" -k -s "https://localhost:9200/_cluster/health?wait_for_status=yellow&timeout=50s" | grep -q '"status":"\(yellow\|green\)"'; do
+  sleep 5
+done
+
+# Apply a policy
+curl -X PUT curl -X PUT "https://localhost:9200/_ilm/policy/my_policy" \
+  -u "elastic:${ELASTIC_PASSWORD}" \
+  -H 'Content-Type: application/json' \
+  -k \
+  -d'
+{
+  "policy": {
+    "phases": {
+      "hot": {
+        "actions": {
+          "rollover": {
+            "max_age": "1m"
+          }
+        }
+      },
+      "cold": {
+        "min_age": "2m",
+        "actions": {
+          "freeze": {}
+        }
+      }
+    }
+  }
+}
+'
+
+wait $ES_PID
