445fa765f3bd8c3a569b0f97553a2085f07dcaf3
Youngho Cho <dominoyh5@gmail.com>
2025-03-03 15:33:48 +0900

    refactor: rename duel route filename

--- backend-ping-pong/src/routes/duel.ts
+++ backend-ping-pong/src/routes/duel.ts
@@ -1,15 +1,15 @@
 import { FastifyPluginAsync } from "fastify"
 import websocket from '@fastify/websocket'
 import { v4 as uuidv4 } from 'uuid'
-import { MatchManager } from "../../../duel/MatchManager"
-import { Player } from "../../../duel/Player"
+import { MatchManager } from "../duel/MatchManager"
+import { Player } from "../duel/Player"
 
 const example: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
     await fastify.register(websocket)
 
     const matchManager = new MatchManager(fastify, 2);
 
-    fastify.get('/ws', { websocket: true }, async (ws) => {
+    fastify.get('/ping-pong/duel/ws', { websocket: true }, async (ws) => {
         const player = new Player("player-" + uuidv4(), uuidv4().substring(0, 2), ws);
         player.send({ type: "wait" })
 
