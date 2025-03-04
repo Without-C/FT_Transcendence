3733ce0dba60af58554d75e3871c39d90bc2f402
somilee <somilee@student.42seoul.kr>
2025-03-04 13:50:05 +0900

    feat: add playpage tailwind css

--- frontend/src/pages/playpage.ts
+++ frontend/src/pages/playpage.ts
@@ -1,9 +1,27 @@
 export function renderPlayPage() {
 	const template = `
-		<nav>
-			<a href="#/play/1p1" data-link>1P1</a>
-			<a href="#/play/mp" data-link>MP</a>
-			<a href="#/play/tp" data-link>TP</a>
+		<nav class="flex flex-col items-end justify-center min-h-screen text-center gap-6">
+			<h2 class="text-3xl font-bold text-white bg-blue-700 py-4 px-10 w-full max-w-3xl">
+				CHOOSE PLAY MODE
+			</h2>
+			<a href="#/play/1p1" data-link class="w-full max-w-3xl bg-[#6C233D] text-left text-white rounded-lg p-6 cursor-pointer">
+				<div class="flex items-center gap-4">
+					<span class="text-5xl font-bold text-pink-300">1P1</span>
+					<div>
+					<h3 class="text-2xl font-bold">ONE-ON-ONE</h3>
+					<p class="text-sm text-gray-300">CHALLENGE A SINGLE OPPONENT IN A HEAD-TO-HEAD BATTLE</p>
+					</div>
+				</div>
+			</a>
+			<a href="#/play/tp" data-link class="w-full max-w-3xl bg-green-800 text-left text-white rounded-lg p-6 cursor-pointer">
+				<div class="flex items-center gap-4">
+					<span class="text-5xl font-bold text-green-300">T P</span>
+					<div>
+						<h3 class="text-2xl font-bold">TOURNAMENT PLAYER</h3>
+						<p class="text-sm text-gray-300">COMPETE IN A STRUCTURED COMPETITION TO BECOME THE CHAMPION</p>
+					</div>
+				</div>
+			</a>
 		</nav>
 	`;
   
