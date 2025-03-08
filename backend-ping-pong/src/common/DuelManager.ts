220fe1f717007f1091d78ae6f316800033af4ef9
Youngho Cho <dominoyh5@gmail.com>
2025-03-08 19:42:10 +0900

    fix: import path

--- backend-ping-pong/src/common/DuelManager.ts
+++ backend-ping-pong/src/common/DuelManager.ts
@@ -1,5 +1,5 @@
 import { Player } from "./Player";
-import { GameEngine } from "../duel/GameEngine";
+import { GameEngine } from "./GameEngine";
 
 export class DuelManager {
     private engine: GameEngine | null = null;
