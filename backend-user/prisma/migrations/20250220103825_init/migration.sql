19e69dcd9d233d569bad48075f03ecbcaaaa9d70
duckgii <shinyonggi@naver.com>
2025-03-09 13:49:09 +0900

    Fix: download avatar_url in backend-user/upload. #25

--- backend-user/prisma/migrations/20250220103825_init/migration.sql
+++ backend-user/prisma/migrations/20250220103825_init/migration.sql
@@ -11,12 +11,11 @@ PRAGMA foreign_keys=OFF;
 CREATE TABLE "new_User" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "username" TEXT NOT NULL,
-    "avatar_url" TEXT NOT NULL,
     "oauth_id_42" TEXT,
     "oauth_id_github" TEXT,
     "oauth_id_google" TEXT
 );
-INSERT INTO "new_User" ("avatar_url", "id", "oauth_id_42", "oauth_id_github", "oauth_id_google", "username") SELECT "avatar_url", "id", "oauth_id_42", "oauth_id_github", "oauth_id_google", "username" FROM "User";
+INSERT INTO "new_User" ("id", "oauth_id_42", "oauth_id_github", "oauth_id_google", "username") SELECT "id", "oauth_id_42", "oauth_id_github", "oauth_id_google", "username" FROM "User";
 DROP TABLE "User";
 ALTER TABLE "new_User" RENAME TO "User";
 PRAGMA foreign_keys=ON;
