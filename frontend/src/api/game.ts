f7b865fa13983426842cbfa74905fa790a1bb0d9
Inimossomini <101785230+Inimossomini@users.noreply.github.com>
2025-03-24 01:16:48 +0900

    feat: add handleAPIError util function

---
+++ frontend/src/api/game.ts
@@ -0,0 +1,1 @@
+import * as GameTypes from "@types/game";
--- frontend/src/api/user.ts
+++ frontend/src/api/user.ts
@@ -1,60 +1,58 @@
 import * as UserTypes from "@/types/user";
+import { handleApiError } from "@/utils/handleApiError";
 
-// 프로필 이미지 가져오기
+/**
+ * 프로필 이미지(avatar_url) 가져오기
+ * @returns avatar_url(string)이 담긴 객체
+ */
 export async function fetchAvatar(): Promise<UserTypes.AvatarResponse> {
 	const res = await fetch("/api/user/mypage/avatar");
 
 	if(!res.ok) { //400
-		try {
-			const error: UserTypes.ApiError = await res.json();
-			throw new Error(error.error_msg || "프로필 이미지 가져오기 실패");
-		} catch {
-			throw new Error("프로필 이미지 가져오기 실패");
-		}
+		await handleApiError(res, "프로필 이미지 가져오기 실패");
 	}
 
 	const data: UserTypes.AvatarResponse = await res.json();
 	return data;
 }
 
-// 유저 이름 가져오기
+/**
+ * 유저 이름(username) 가져오기
+ * @returns username(string)이 담긴 객체
+ */
 export async function fetchUsername(): Promise<UserTypes.UsernameResponse> {
 	const res = await fetch("/api/user/mypage/username");
 
 	if(!res.ok) {
-		try {
-			const error: UserTypes.ApiError = await res.json();
-			throw new Error(error.error_msg || "유저 이름 가져오기 실패");
-		} catch {
-			throw new Error("유저 이름 가져오기 실패");
-		}
+		await handleApiError(res, "유저 이름 가져오기 실패");
 	}
 
 	const data: UserTypes.UsernameResponse = await res.json();
 	return data;
 }
 
-// 프로필 이미지 변경하기 -> string이여도 괜찮나, FormData??
+/**
+ * 프로필 이미지 변경하기 -> string이여도 괜찮나, FormData??
+ * @param newAvatarUrl 새로 변경할 프로필 이미지 URL
+ */
 export async function updateAvatar(newAvatarUrl: string): Promise<void> {
 	const res = await fetch("/api/user/mypage/avatar", {
 		method: "PATCH",
 		headers: {
-			"Content-Types": "application/json",
+			"Content-Type": "application/json",
 		},
 		body: JSON.stringify({avatar_url: newAvatarUrl}),
 	});
 
-	if (!res.ok) {
-		try {
-			const error: UserTypes.ApiError = await res.json();
-			throw new Error(error.error_msg || "프로필 이미지 변경 실패");
-		} catch {
-			throw new Error("프로필 이미지 변경 실패");
-		}
+	if(!res.ok) {
+		await handleApiError(res, "프로필 이미지 변경 실패");
 	}
 }
 
-// 유저 이름 변경하기
+/**
+ * 유저 이름 변경하기
+ * @param newUsername 새로 설정할 유저 이름
+ */
 export async function updateUsername(newUsername: string): Promise<void> {
 	const res = await fetch("/api/user/mypage/username", {
 		method: "PATCH",
@@ -64,51 +62,44 @@ export async function updateUsername(newUsername: string): Promise<void> {
 		body: JSON.stringify({username: newUsername}),
 	});
 
-	if (!res.ok) {
-		try {
-			const error: UserTypes.ApiError = await res.json();
-			throw new Error(error.error_msg || "유저 이름 변경 실패");
-		} catch {
-			throw new Error("유저 이름 변경 실패");
-		}
+	if(!res.ok) {
+		await handleApiError(res, "유저 이름 변경 실패");
 	}
 }
 
-// 팔로잉 목록 가져오기
+/**
+ * 팔로잉 목록 가져오기
+ * @returns username과 online 상태가 담긴 유저 배열 or 빈 객체 -> online은 일단 모두 1
+ */
 export async function fetchFollowing(): Promise<UserTypes.FollowingResponse> {
 	const res = await fetch("/api/user/mypage/following");
 
 	if(!res.ok) {
-		try {
-			const error: UserTypes.ApiError = await res.json();
-			throw new Error(error.error_msg || "팔로잉 목록 가져오기 실패");
-		} catch {
-			throw new Error("팔로잉 목록 가져오기 실패");
-		}
+		await handleApiError(res, "팔로잉 목록 가져오기 실패");
 	}
 
 	const data: UserTypes.FollowingResponse = await res.json();
 	return data;
 }
-
-// 팔로워 명수 가져오기
+/**
+ * 팔로워 명수 가져오기
+ * @returns following_number(number)이 담긴 객체
+ */
 export async function fetchFollow(): Promise<UserTypes.FollowResponse> {
 	const res = await fetch("/api/user/mypage/followers");
 
 	if(!res.ok) {
-		try {
-			const error: UserTypes.ApiError = await res.json();
-			throw new Error(error.error_msg || "팔로워 명수 가져오기 실패");
-		} catch {
-			throw new Error("팔로워 명수 가져오기 실패");
-		}
+		await handleApiError(res, "팔로워 명수 가져오기 실패");
 	}
 
 	const data: UserTypes.FollowResponse = await res.json();
 	return data;
 }
 
-// 특정 사용자 팔로우하기
+/**
+ * 특정 사용자 팔로우하기
+ * @param username 팔로우할 유저 이름
+ */
 export async function followUser(username: string): Promise<void> {
 	const res = await fetch("/api/user/mypage/follow", {
 		method: "POST",
@@ -119,17 +110,15 @@ export async function followUser(username: string): Promise<void> {
 	});
 
 	if(!res.ok) {
-		try {
-			const error: UserTypes.ApiError = await res.json();
-			throw new Error(error.error_msg || "팔로우 실패");
-		} catch {
-			throw new Error("팔로우 실패");
-		}
+		await handleApiError(res, "팔로우 실패");
 	}
 }
 
-// 특정 사용자 언팔로우
-export async function UnfollowUser(username: string): Promise<void> {
+/**
+ * 특정 사용자 언팔로우
+ * @param username 언팔로우할 유저 이름
+ */
+export async function unfollowUser(username: string): Promise<void> {
 	const res = await fetch("/api/user/mypage/unfollow", {
 		method: "DELETE",
 		headers: {
@@ -139,27 +128,21 @@ export async function UnfollowUser(username: string): Promise<void> {
 	});
 
 	if(!res.ok) {
-		try {
-			const error: UserTypes.ApiError = await res.json();
-			throw new Error(error.error_msg || "언팔로우 실패");
-		} catch {
-			throw new Error("언팔로우 실패");
-		}
+		await handleApiError(res, "언팔로우 실패");
 	}
 }
 
-// 특정 문자로 시작하는 유저 목록 가져오기
-export async function searchUser(keyword: string): Promise<UserTypes.SearchResponse> {
+/**
+ * 특정 문자로 시작하는 유저 목록 가져오기
+ * @param keyword 검색할 유저 이름의 시작 문자열
+ * @returns username, online, following 상태가 담긴 유저 배열 or 빈 객체 
+ */
+export async function searchUsers(keyword: string): Promise<UserTypes.SearchResponse> {
 	const query = encodeURIComponent(keyword);
 	const res = await fetch(`/api/user/mypage/search?searching_user=${query}`);
 
 	if(!res.ok) {
-		try {
-			const error: UserTypes.ApiError = await res.json();
-			throw new Error(error.error_msg || "유저 검색 실패");
-		} catch {
-			throw new Error("유저 검색 실패");
-		}
+		await handleApiError(res, "유저 검색 실패");
 	}
 
 	const data: UserTypes.SearchResponse = await res.json();
